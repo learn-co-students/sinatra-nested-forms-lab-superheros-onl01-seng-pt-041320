4c56c8926c436cf3253033c203c67d982c51ff10
vedransehovic <vedran.sehovic@gmail.com>
2020-06-28 19:42:43 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        
+        #view
+        erb :form
+    end
+
+    post '/teams' do
+        
+    end
 
 end
